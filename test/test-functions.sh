c6dc3a83236e91f8ba2c20a10c43b1e97641606c
Christian Garbs <mitch@cgarbs.de>
2019-07-25 22:08:02 +0200

    test all parameter validation errors

--- test/test-functions.sh
+++ test/test-functions.sh
@@ -18,4 +18,63 @@ setup_test()
     chmod +x "$BINARY"
 }
 
+# $1 = client IP address from SSH connection
+set_ssh_client_ip()
+{
+    export SSH_CONNECTION=$1
+}
+
+# $1 = hostname (second word of "command=" in .ssh/authorized_keys entry)
+# $2 = ip address (passed to script, defaults to auto)
+# $3 = script name (first word of "command=" in .ssh/authorized_keys entry, defaults to /home/dns-update/update-client)
+script_under_test()
+{
+    export SSH_ORIGINAL_COMMAND="${3:-/home/dns-update/update-client} $2"
+    "$BINARY" "$1"
+}
+
+# $1 = file to check
+# $2 = expected content
+assert_contains()
+{
+    local FILE="$1" FILENAME="${1##*/}" EXPECTED="$2"
+    if [ ! -e "$FILE" ]; then
+	echo "!!! file '$FILENAME' to be checked not found"
+	exit 1
+    fi
+    if ! grep -q "$EXPECTED" "$FILE"; then
+	echo "!!! $FILENAME does not contain '$EXPECTED' but:"
+	echo "!!! -- START --"
+	cat "$FILE"
+	echo "!!! --- END ---"
+	exit 1
+    fi
+}
+
+# $* = command to execute
+expect_error()
+{
+    if "$@"; then
+	echo "!!! command finished successfully but was expected to fail"
+	exit 1
+    fi
+}
+
+# $1 = expected error message
+# $* = command to execute
+expect_error_message()
+{
+    local EXPECTED="$1"
+    shift 1
+    OUTPUT="$TESTDIR/output"
+    if "$@" > "$OUTPUT"; then
+	echo "$ERRMSG"
+	exit 1
+    fi
+    assert_contains "$OUTPUT" "$EXPECTED"
+    cat "$OUTPUT"
+}
+
+
+
 setup_test
